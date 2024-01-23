e5d956d501290dab4ba91284c70992fccfec9871
bhuvaneshwa <bhuvaneshwaran368@gmail.com>
2024-01-23 17:02:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hello from './Hello'
+import Myclass from './Myclass'
+import Ter from './Ter'
+
+
+export default function App() {
+  return (
+    <div>
+      <Hello/>
+      <Myclass/>
+      <Ter/>
+
+    </div>
+  )
+}
